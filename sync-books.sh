f86dc1b1c141419b36e1547ea2a235ea3f6dd30d
Patrick Gaskin <patrick@pgaskin.net>
2020-02-20 15:31:44 -0500

    Updated scripts, dictionaries

--- sync-books.sh
+++ sync-books.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-kepubify --update --output "$(kobo-find -w -f)/kepubify" "$HOME/Documents/Books/Books/"
+kepubify --update --output "$(kobo-find -w -f)/kepubify/Books_converted/" "$HOME/Documents/Books/Books/"
 seriesmeta
 covergen -a 1.5
+dictutil install -lgt -nGOTDict "$HOME/Documents/Books/Dictionaries/gotdict.zip"
+dictutil install -lox -nOxford "$HOME/Documents/Books/Dictionaries/oxford.zip"
