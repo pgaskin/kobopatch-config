c708128554bb1b4c512a1be725d7753f288b573a
Patrick Gaskin <patrick@pgaskin.net>
2020-02-29 12:52:28 -0500

    Updated sync script

--- sync-books.sh
+++ sync-books.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kepubify --update --output "$(kobo-find -w -f)/kepubify/Books_converted/" "$HOME/Documents/Books/Books/"
+dicthtml I -lgt -nGOTDict "$HOME/Documents/Books/.kobodict/gotdict.zip"
+dicthtml I -lox -nOxford "$HOME/Documents/Books/.kobodict/oxford.zip"
+kepubify -u -o"$(kobo-find -w -f)/kepubify/" -x.pdf "${HOME}/Documents/Books/"{Books,Other}"/"
+covergen -a1.5
 seriesmeta
-covergen -a 1.5
-dictutil install -lgt -nGOTDict "$HOME/Documents/Books/Dictionaries/gotdict.zip"
-dictutil install -lox -nOxford "$HOME/Documents/Books/Dictionaries/oxford.zip"
