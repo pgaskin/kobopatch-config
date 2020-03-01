168597c90d9fc5de9b06ad1a1d0c91da1e9c81a4
Patrick Gaskin <patrick@pgaskin.net>
2020-03-01 12:33:58 -0500

    Fixed typo in script

--- sync-books.sh
+++ sync-books.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dicthtml I -lgt -nGOTDict "$HOME/Documents/Books/.kobodict/gotdict.zip"
-dicthtml I -lox -nOxford "$HOME/Documents/Books/.kobodict/oxford.zip"
+dictutil I -lgt -nGOTDict "$HOME/Documents/Books/Books/.kobodict/gotdict.zip"
+dictutil I -lox -nOxford "$HOME/Documents/Books/Books/.kobodict/oxford.zip"
 kepubify -u -o"$(kobo-find -w -f)/kepubify/" -x.pdf "${HOME}/Documents/Books/"{Books,Other}"/"
 covergen -a1.5
 seriesmeta
