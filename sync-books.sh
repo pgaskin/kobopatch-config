6320794c77ae556a78c33f14378cab49a7520b83
Patrick Gaskin <patrick@pgaskin.net>
2020-07-19 21:43:52 -0400

    Added NickelSeries (pgaskin/kobo-mods@8c5d318d5b7da721ae61026cf9dafac891945427)

--- sync-books.sh
+++ sync-books.sh
@@ -4,4 +4,4 @@ dictutil I -lox -nOxford "$HOME/Documents/Books/Books/.kobodict/oxford.zip"
 dictutil I -lwb -nWebster1913 "$HOME/Documents/Books/Books/.kobodict/webster1913.zip"
 kepubify -u -o"$(kobo-find -w -f)/kepubify/" -x.pdf "${HOME}/Documents/Books/"{Books,Other}"/"
 covergen -a1.5
-seriesmeta
+#seriesmeta
